7832bf4e7edab2d6880ac7bd13c8d63f8e178075
asterid-leaper <asteridleaper@gmail.com>
2024-10-23 22:10:55 -0700

    Backing up Minigame Manager
    -WIP

---
+++ Assets/ScriptableObjects/MinigameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameManager : MonoBehaviour
+{
+    [SerializeField] private MinigameScriptableObject minigameTemplate; // Template object to instantiate
+
+
+    
+    //List of minigames apart of the 'Party' Category
+    public List<MinigameScriptableObject> PartyMinigames{
+        get {return partyMinigames;}
+    }
+    private List<MinigameScriptableObject> partyMinigames;
+
+    private void Awake()
+    {
+        // Initialize the list and create instances using collection initializer syntax
+        partyMinigames = new List<MinigameScriptableObject>
+        {
+            CreateMinigame("Poppin' Rush", "Pop the balloons before they leave the screen", "Party", null),
+            CreateMinigame("Snip and Shine", "Cut the folded paper along the dotted lines", "Party", null),
+        };
+    }
+
+    private MinigameScriptableObject CreateMinigame(string levelName, string description, string category, Sprite previewImage)
+    {
+        // Instantiate the template MinigameScriptableObject
+        MinigameScriptableObject newMinigame = Object.Instantiate(minigameTemplate);
+        
+        // Set the unique values for this instance
+        newMinigame.LevelName = levelName;
+        newMinigame.Description = description;
+        newMinigame.Category = category;
+        newMinigame.PreviewImage = previewImage;
+        
+        return newMinigame;
+    }
+}
--- Assets/ScriptableObjects/MinigameScriptableObject.cs
+++ Assets/ScriptableObjects/MinigameScriptableObject.cs
@@ -6,5 +6,28 @@ using UnityEngine;
 //menuName = "ScriptableObjects/Minigame")]
 public class MinigameScriptableObject : ScriptableObject
 {
-    
+    private string levelName;
+    private string levelDescription;
+    private string category;
+    private Sprite previewImage;
+
+    public string LevelName{
+        get{ return levelName; }
+        set{levelName = value;}
+    }
+
+    public string LevelDescription{
+        get{ return levelDescription; }
+        set{levelDescription = value;}
+    }
+
+    public Sprite PreviewImage{
+        get{ return previewImage; }
+        set{previewImage = value;}  
+    }
+
+    public string Category{
+        get{ return category; }
+        set{category = value;}
+    }
 }
